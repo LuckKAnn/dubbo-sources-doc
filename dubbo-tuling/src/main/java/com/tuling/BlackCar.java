5493c21030f22d424753b6d4e7f6b8000fc215d5
LuckKAnn <1546165200@qq.com>
2022-04-19 20:09:24 +0800

    add notes in all without deatail in 20220419

---
+++ dubbo-tuling/src/main/java/com/tuling/BlackCar.java
@@ -0,0 +1,13 @@
+package com.tuling;
+
+import org.apache.dubbo.common.URL;
+
+public class BlackCar implements Car {
+
+    @Override
+    public String getCarName(URL url) {
+        return "black";
+    }
+
+
+}
---
+++ dubbo-tuling/src/main/java/com/tuling/RedCar.java
@@ -0,0 +1,19 @@
+package com.tuling;
+
+import org.apache.dubbo.common.Extension;
+import org.apache.dubbo.common.URL;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedCar implements Car {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public String getCarName(URL url) {
+        return "red";
+    }
+
+}
